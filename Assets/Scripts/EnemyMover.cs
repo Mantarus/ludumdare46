993ca7d687a4788d01eaed5d7281bc69f206fcd9
Mantarus <m.mantarus@gmail.com>
2020-04-20 18:18:08 +0300

    Add attack animation and more stuff

---
+++ Assets/Scripts/EnemyMover.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+public class EnemyMover : MonoBehaviour
+{
+    public Transform player;
+    public Transform bed;
+
+    public float speed;
+
+    private Transform _target;
+    private Rigidbody2D _rb;
+    private bool _facingRight = true;
+
+    private void Start()
+    {
+        _target = player;
+        _rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        var direction = Mathf.Sign((_target.position - transform.position).x);
+        _rb.velocity = Vector2.right * (direction * speed);
+
+        if (direction > 0 && !_facingRight || direction < 0 && _facingRight)
+        {
+            Flip();
+        }
+    }
+    
+    private void Flip()
+    {
+        // Switch the way the player is labelled as facing.
+        _facingRight = !_facingRight;
+
+        // Multiply the player's x local scale by -1.
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+}
---
+++ Assets/Scripts/PlayerCombat.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class PlayerCombat : MonoBehaviour
+{
+    public Animator characterAnimator;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            characterAnimator.SetTrigger("Attack");
+        }
+    } 
+}
